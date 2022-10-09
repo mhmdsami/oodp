92ae090835ce3586ba8c1d8b2950f836b8b3a7cd
Balaram <balaramreddy1710@gmail.com>
2022-10-09 19:25:22 +0530

    Added new problem

---
+++ Classes_Methods_Constructors/Level_1/change-base.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class ChangeBase{
+    public:
+    int n,k,rem=0;
+    void sumBase(int n,int k){
+       while(n!=0){
+           rem+=(n%k);
+           n/=k;
+       } 
+       cout << rem;
+    }
+};
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    ChangeBase Convert;
+    Convert.sumBase(n,k);
+	return 0;
+}
