093e848cfc2e3c445620e72f34a9a04dc41b427b
rv1527 <112467462+rv1527@users.noreply.github.com>
2024-07-10 13:13:04 +0530

    refactor: better variable name (#13)

--- Operator_Overloading/Level_1/difference.cpp
+++ Operator_Overloading/Level_1/difference.cpp
@@ -6,20 +6,20 @@ class Diff
     int n;
 
 public:
-    int sumofsquare();
-    int squareofsum();
+    int sum_of_square();
+    int square_of_sum();
     friend void operator>>(istream &in, Diff &obj)
     {
         cin >> obj.n;
     }
 };
 
-int Diff::sumofsquare()
+int Diff::sum_of_square()
 {
     return n * (n + 1) * (2 * n + 1) / 6;
 }
 
-int Diff::squareofsum()
+int Diff::square_of_sum()
 {
     return n * n * (n + 1) * (n + 1) / 4;
 }
@@ -28,6 +28,6 @@ int main()
 {
     Diff d;
     cin >> d;
-    cout << d.squareofsum() - d.sumofsquare();
+    cout << d.square_of_sum() - d.sum_of_square();
     return 0;
 }
